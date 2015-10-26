47011ab69e6db5303a35792872acc558a53a74c6
Альберт Степанцев <126@gosbroker.ru>
2015-10-26 15:53:08 +0300

    + config

--- public/index.php
+++ public/index.php
@@ -4,4 +4,9 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../protected/boot.php';
 require __DIR__ . '/../protected/autoload.php';
 
-T4\Mvc\Application::getInstance()->run();
+T4\Mvc\Application
+    ::getInstance()
+    ->setConfig(
+        new \T4\Core\Config(ROOT_PATH_PROTECTED . '/config.php')
+    )
+    ->run();
