458b9638edc5f2d0dce362950fa25fda3a792acf
Альберт Степанцев <126@gosbroker.ru>
2016-04-11 10:44:23 +0300

    ! T4 update

--- protected/t4.php
+++ protected/t4.php
@@ -5,6 +5,9 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/boot.php';
 require __DIR__ . '/autoload.php';
 
-\T4\Console\Application
+T4\Console\Application
     ::instance()
+    ->setConfig(
+        new \T4\Core\Config(ROOT_PATH_PROTECTED . '/config.php')
+    )
     ->run();
